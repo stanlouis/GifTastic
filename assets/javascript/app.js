c40a790cfbd5a87bbd15e1f19a63c53680bcdebe
Stanley Louis <stanlouis@msn.com>
2018-06-04 17:57:24 -0400

    Change some variables names

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
-// Initial array of animals
-let animals = ["parrot", "dog", "lion", "turtle"];
+// Initial array
+let topics = ["parrot", "dog", "lion", "turtle"];
 
 let generateDomEl = function(results) {
   // Looping through each result item
   results.forEach(result => {
     // Creating and storing a div tag
-    const animalDiv = $("<div class='col-md-4 bg-light card text-dark'>");
+    const nameDiv = $("<div class='col-md-4 bg-light card text-dark'>");
 
     // creating a title tag
     const titleEl = result.title.replace("GIF", "").trim();
@@ -19,34 +19,34 @@ let generateDomEl = function(results) {
     );
 
     // Creating and storing an image tag
-    const animalImage = $("<img class='card-img-bottom gif gif-style'>");
+    const nameImage = $("<img class='card-img-bottom gif gif-style'>");
     // Setting the src attribute of the image to a property pulled off the result item
-    animalImage.attr("src", result.images.fixed_height_still.url);
+    nameImage.attr("src", result.images.fixed_height_still.url);
     // Add data attribute for still, state and animate
-    animalImage.attr({
+    nameImage.attr({
       "data-still": result.images.fixed_height_still.url,
       "data-animate": result.images.fixed_height.url,
       "data-state": "still"
     });
 
-    // Appending the paragraph and image tag to the animalDiv
-    animalDiv
+    // Appending the paragraph and image tag to the nameDiv
+    nameDiv
       .append(title)
       .append(p)
-      .append(animalImage);
+      .append(nameImage);
 
-    // Pre-pending the animalDiv to the HTML page in the "#gifs-appear-here" div
-    $("#animals-view").prepend(animalDiv);
+    // Pre-pending the nameDiv to the HTML page in the "#gifs-appear-here" div
+    $("#topics-view").prepend(nameDiv);
   });
 };
 
-function displayAnimalInfo() {
-  let animal = $(this).attr("data-name");
+function displayNameInfo() {
+  let name = $(this).attr("data-name");
   // Storing our giphy API URL
-  let giphyQueryURL = `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=11CRvXftTpN39YgVEj92UDDmdFbg1JVi&limit=10`;
+  let giphyQueryURL = `https://api.giphy.com/v1/gifs/search?q=${name}&api_key=11CRvXftTpN39YgVEj92UDDmdFbg1JVi&limit=10`;
 
-  // Clear animalDiv
-  $("#animals-view").empty();
+  // Clear nameDiv
+  $("#topics-view").empty();
 
   // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
@@ -55,8 +55,8 @@ function displayAnimalInfo() {
   }).then(function(response) {
     let results = response.data;
 
-    // Creating a div to hold the animal
-    //  let animalDiv = $("<div class='card'>");
+    // Creating a div to hold the name
+    //  let nameDiv = $("<div class='card'>");
 
     generateDomEl(results);
 
@@ -75,40 +75,42 @@ function displayAnimalInfo() {
 
 // Function for displaying buttons
 function renderButtons() {
-  // Deleting the animals prior to adding new animals
+  // Deleting the topics prior to adding new topics
   $("#buttons-view").empty();
 
-  // Adding a button with classes and data-attributes for each animal
-  animals.forEach(animal => {
+  // Adding a button with classes and data-attributes for each name
+  topics.forEach(name => {
     const a = $("<button>");
-    a.addClass("btn btn-info animal-btn");
-    a.attr("data-name", animal);
-    a.text(animal);
+    a.addClass("btn btn-info name-btn");
+    a.attr("data-name", name);
+    a.text(name);
     $("#buttons-view").append(a);
   });
 }
 
 // Event listener to receive input value for buttons
-$("#add-animal").on("click", event => {
+$("#add-name").on("click", event => {
   event.preventDefault();
 
-  let input = $("#animal-input");
-  const animal = input.val().trim();
+  let input = $("#name-input");
+  const name = input.val().trim();
 
-  if (!animals.includes(animal)) {
-    animals.push(animal);
+  if (name === "") {
+    alert("A name is required");
+  } else if (!topics.includes(name)) {
+    topics.push(name);
     input.val("");
   } else {
     input.val("");
     alert("Please provide another name");
   }
 
-  // Calling renderButtons which handles the processing of our animal array
+  // Calling renderButtons which handles the processing of our name array
   renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "animal-btn"
-$(document).on("click", ".animal-btn", displayAnimalInfo);
+// Adding a click event listener to all elements with a class of "name-btn"
+$(document).on("click", ".name-btn", displayNameInfo);
 
 // Calling the renderButtons function to display the initial buttons
 renderButtons();
